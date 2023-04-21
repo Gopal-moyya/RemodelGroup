4c0b1346ca0f04247e3ffca7a3b7f8307f61f567
gvamshi-metrological <vamshikrishna.gurram@soctronics.com>
2023-04-21 14:51:00 +0530

    Added static data file, so that all can use it for data.

---
+++ remodelgroup/src/lib/staticData.js
@@ -0,0 +1,27 @@
+export const products = {
+        bannerImage: require('@/assets/products/product-banner.jpg'),
+        title: 'Our Products',
+        tagline: 'Tagline for the banner',
+        desc1: '',
+        desc2: '',
+        products: [
+            {
+                title: 'Product 1',
+                id: 'product1',
+                desc: 'product description',
+                logo: require('@/assets/products/product1.png'),
+            },
+            {
+                title: 'Product 2',
+                id: 'product2',
+                desc: 'product description',
+                logo: require('@/assets/products/product2.png'),
+            },
+            {
+                title: 'Product 3',
+                id: 'product3',
+                desc: 'product description',
+                logo: require('@/assets/products/product3.png'),
+            },
+        ]
+    }
